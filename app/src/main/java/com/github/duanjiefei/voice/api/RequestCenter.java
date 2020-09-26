b804409c1440effdd7917c10350e4176a1b9d78a
duanjiefei <jiefeiduan@163.com>
2020-09-26 09:04:04 +0800

    去掉主工程中的登录 请求

--- app/src/main/java/com/github/duanjiefei/voice/api/RequestCenter.java
+++ app/src/main/java/com/github/duanjiefei/voice/api/RequestCenter.java
@@ -23,11 +23,6 @@ public class RequestCenter {
         private static String HOME_RECOMMAND_MORE = ROOT_URL + "/module_voice/home_recommand_more";
 
         private static String HOME_FRIEND = ROOT_URL + "/module_voice/home_friend";
-
-        /**
-         * 登陆接口
-         */
-        public static String LOGIN = ROOT_URL + "/module_voice/login_phone";
     }
 
     //根据参数发送所有post请求
@@ -49,17 +44,6 @@ public class RequestCenter {
 
     public static void requestFriendData(DisposeDataListener listener) {
         RequestCenter.getRequest(HttpConstants.HOME_FRIEND, null, listener, BaseFriendModel.class);
-    }
-
-    /**
-     * 用户登陆请求
-     */
-    public static void login(DisposeDataListener listener) {
-
-        RequestParams params = new RequestParams();
-        params.put("mb", "18734924592");
-        params.put("pwd", "999999q");
-        RequestCenter.getRequest(HttpConstants.LOGIN, params, listener, User.class);
     }
 }
 
